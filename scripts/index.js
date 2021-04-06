ee90b245cbcd089d67733dd1f472ecbefbddeb59
Evgeniy Guk <jekiee007@yandex.by>
2021-04-06 08:59:06 +0300

    Слияние веток

--- scripts/index.js
+++ scripts/index.js
@@ -10,12 +10,6 @@ let popupExitBtn = formElement.querySelector(".popup__close");
 let popupNameInput = formElement.querySelector(".popup-data__input_type_name");
 let popupJobInput = formElement.querySelector(".popup-data__input_type_job");
 
-<<<<<<< HEAD
-let createNewCard = document.querySelector("#placeCard");
-
-let cardContainer = document.querySelector(".places");
-let newPlace = cardContainer.querySelector(".popup__card-editor");
-=======
 let placeCard = document.querySelector("#placeCard");
 
 let cardContainer = document.querySelector(".places");
@@ -31,7 +25,6 @@ let popupCardURL = newPlace.querySelector(".popup-data__input_type_url");
 let popupImage = document.querySelector("#popupImage");
 let popupZoomedImage = popupImage.querySelector(".popup__zoomed-picture");
 let popupPictureCaption = popupImage.querySelector(".popup__picture-caption");
->>>>>>> 8ce0733e9381e41b47b0f7a3d999606304807431
 
 // хардкод карточек (сугубо для проверки :) )
 const initialCards = [
@@ -68,21 +61,6 @@ const initialCards = [
 ];
 
 // Функция создания карточкти
-<<<<<<< HEAD
-function createNewCardFun(data) {
-  let newCard = createNewCard.content.cloneNode(true);
-  let placeName = newCard.querySelector(".card__place-name");
-
-  placeName.textContent = data.name;
-
-  let cardImege = newCard.querySelector(".card__image");
-  cardImege.alt = data.name;
-  cardImege.src = data.link;
-
-  let cardRemoveBtn = newCard.querySelector(".card__remove");
-
-  let likeBtn = newCard.querySelector(".card__like");
-=======
 function createNewCard(item) {
   let newCard = placeCard.content.cloneNode(true);
   let placeName = newCard.querySelector(".card__place-name");
@@ -106,18 +84,10 @@ function createNewCard(item) {
   likeBtn.addEventListener("click", (evt) =>
     evt.target.classList.toggle("card__like_active")
   );
->>>>>>> 8ce0733e9381e41b47b0f7a3d999606304807431
 
   return newCard;
 }
 
-<<<<<<< HEAD
-// инициализация карточки
-function initCard() {
-  let card = initialCards.map(function (item) {
-    return createNewCardFun(item);
-  });
-=======
 // попап увеличенной картинки
 function popupImageOpen(item) {
   console.log(item);
@@ -134,33 +104,16 @@ function popupImageOpen(item) {
 // инициализация карточки
 function initCard() {
   let card = initialCards.map((item) => createNewCard(item));
->>>>>>> 8ce0733e9381e41b47b0f7a3d999606304807431
 
   cardContainer.append(...card);
 }
 
-<<<<<<< HEAD
-let popupCardTitle = cardContainer.querySelector(
-  ".popup-data__input_type_title"
-);
-let popupCardURL = cardContainer.querySelector(".popup-data__input_type_url");
-// let popupNameInput = formElement.querySelector(".popup-data__input_type_name");
-// let popupJobInput = formElement.querySelector(".popup-data__input_type_job");
-
-// попап карточки
-function openCardPopup() {
-  popupCardTitle.value = "Название";
-  popupCardURL.value = "Ссылка на картинку";
-
-  formElement.classList.add("popup_opened");
-=======
 // попап карточки
 function openCardPopup() {
   popupCardTitle.value = null;
   popupCardURL.value = null;
 
   newPlace.classList.add("popup_opened");
->>>>>>> 8ce0733e9381e41b47b0f7a3d999606304807431
 }
 
 // попап профиля
@@ -188,8 +141,6 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-<<<<<<< HEAD
-=======
 //создать еще одну карточку
 function createOneMoreCard(evt) {
   evt.preventDefault();
@@ -206,19 +157,15 @@ function createOneMoreCard(evt) {
   closePopup();
 }
 
->>>>>>> 8ce0733e9381e41b47b0f7a3d999606304807431
 initCard();
 
 profileInfoEditBtn.addEventListener("click", openPopup); //открыть попап профиля
 
 popupAddPlaceBtn.addEventListener("click", openCardPopup); //открыть попап создания новой карточки
-<<<<<<< HEAD
-=======
 
 popupExitBtn.addEventListener("click", closePopup); //закрыть попап профиля без сохранения
 
 popupCardExitBtn.addEventListener("click", closePopup); //закрыть попап карточки без сохранения
->>>>>>> 8ce0733e9381e41b47b0f7a3d999606304807431
 
 addNewCard.addEventListener("submit", createOneMoreCard); //закрыть попап карточки с применением изменений
 
