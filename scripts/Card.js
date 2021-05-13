9635fb891d8ef3de2d455ebdb8d0660eed359161
Evgeniy Guk <jekiee007@yandex.by>
2021-05-13 22:43:27 +0300

    Working at home

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,14 +1,26 @@
 export class Card {
+  
   constructor(data, cardSelector, onClick) {
     this._text = data.name;
     this._link = data.link;
     this._cardSelector = cardSelector;
-    this._onClick = onClick;
+    this._handlePreviewPicture = onClick;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    return cardElement;
   }
 
   // метод создания карточки
   getView() {
-    const newCard = this._cardSelector.content.cloneNode(true);
+    const newCard = this._getTemplate();
+    // const newCard = this._cardSelector.content.cloneNode(true);
+
     const placeName = newCard.querySelector(".card__place-name");
     const cardImage = newCard.querySelector(".card__image");
     const cardRemoveBtn = newCard.querySelector(".card__remove");
@@ -19,34 +31,41 @@ export class Card {
     cardImage.alt = this._text;
     cardImage.src = this._link;
 
+    this._setEventListeners(cardImage, cardRemoveBtn, likeBtn);
+
+    return newCard;
+  }
+
+  _setEventListeners(cardImage, cardRemoveBtn, likeBtn) {
     //открыть попап просмотр картинки
-    cardImage.addEventListener("click", this._popupZoomedImg.bind(this));
+    cardImage.addEventListener("click", () => {
+      this._popupZoomedImg();
+    });
+    // cardImage.addEventListener("click", this._popupZoomedImg.bind(this));
 
     //удаление карточки
     cardRemoveBtn.addEventListener("click", this._cardDelete);
 
     //лайк карточки
     likeBtn.addEventListener("click", this._likeCard);
-
-    return newCard;
   }
 
   // метод просмотра картинки
   _popupZoomedImg() {
     const imageData = {
-      link : this._link,
-      name : this._text,
+      link: this._link,
+      name: this._text,
     };
-    this._onClick(imageData);
+    this._handlePreviewPicture(imageData);
+  }
+
+  // метод удаления карточки
+  _cardDelete(evt) {
+    evt.target.closest(".card").remove();
   }
 
   // метод лайк карточки
   _likeCard(evt) {
     evt.target.classList.toggle("card__like_active");
-  }
-
-  // метод удаления карточки
-  _cardDelete(evt) { 
-    evt.target.closest(".card").remove();
   }
 }
